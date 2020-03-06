14066594b26a190a13e782933c5447a3c0ca6134
OSCAR <ojimenez@softcorpcr.com>
2020-03-05 20:48:33 -0600

    Updated copyright

--- src/main/java/com/softcorp/cr/facturaelectronica/api/interceptors/AuditInterceptor.java
+++ src/main/java/com/softcorp/cr/facturaelectronica/api/interceptors/AuditInterceptor.java
@@ -1,6 +1,4 @@
-/*
- * Copyright (c) 2019.  SOFTCORP-CR S.A. All Rights Reserved. Any partial or total reproduction of this file without authorization from the owner is prohibited.
- */
+
 
 package com.softcorp.cr.facturaelectronica.api.interceptors;
 
